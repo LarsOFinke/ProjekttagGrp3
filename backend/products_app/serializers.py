44678bf631db0df811454ed27be566d7f3b57001
ARYAN MASSOUDY <116915753+aryan-massoudy@users.noreply.github.com>
2025-05-09 12:17:17 +0200

    Documentation Added

--- backend/products_app/serializers.py
+++ backend/products_app/serializers.py
@@ -35,16 +35,16 @@ class ProductSerializer(serializers.ModelSerializer):
         stock_data = validated_data.pop('stock', None)
 
         # Update product fields
-        instance.name = validated_data.get('name', instance.name)
+        instance.name = validated_data.get('name', instance.name) # Aktualisiert den Namen, falls im validated_data vorhanden, ansonsten behält er den alten Namen bei.
         instance.short_description = validated_data.get('short_description', instance.short_description)
         instance.product_description = validated_data.get('product_description', instance.product_description)
         instance.price = validated_data.get('price', instance.price)
         instance.save()
 
-        # Update stock if provided
+        # Lagerbestand aktualisieren, falls vorhanden
         if stock_data:
             stock = instance.stock
             stock.quantity = stock_data.get('quantity', stock.quantity)
             stock.save()
 
-        return instance
+        return instance  # Gibt die aktualisierte Produktinstanz zurück.
