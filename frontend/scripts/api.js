ad57ba9b16802bee2f770d4ca29e5d0f0eab2c7f
Lars-Oliver Finke <dev.Lars-Finke@web.de>
2025-05-09 12:00:36 +0200

    Prepared api-fetching data

--- frontend/scripts/api.js
+++ frontend/scripts/api.js
@@ -1,5 +1,24 @@
-export function get_current_stock() {
-  let current_stock = xxx;
+import { environment } from "../environments/test.environment";
 
-  return current_stock;
+const api_url = environment.api_url;
+
+export async function fetch_data(category) {
+  data = [];
+
+  return fetch(`${api_url}get-topics`, {
+    method: "GET",
+    headers: {
+      "Content-Type": "application/json",
+    },
+  })
+    .then((response) => response.json())
+    .then((data) => {
+      data = data.data;
+    })
+    .catch((error) => {
+      createErrorBox(
+        `${category} konnten nicht gefetcht werden! Verbindung zum Server steht?`
+      );
+      console.error("Error:", error);
+    });
 }
