b909c9e01dff277b96496c45940ad2953027f21e
Lars-Oliver Finke <dev.Lars-Finke@web.de>
2025-05-09 11:48:38 +0200

    Added environments

--- frontend/scripts/models/Product.js
+++ frontend/scripts/models/Product.js
@@ -1,1 +1,7 @@
-export class Product {}
+export class Product {
+  constructor(dish, image, description) {
+    this.dish = dish;
+    this.image = image;
+    this.description = description;
+  }
+}
